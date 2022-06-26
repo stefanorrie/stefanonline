48508465999ac0d670e23d1b614ee9453f4b2ac0
stefanorrie <stefanorri001@gmail.com>
2022-06-26 16:48:11 +0000

    header ready for animating potentially

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,10 +3,18 @@ import Link from "next/link";
 
 export const Header = () => {
     return (
-        <div className="flex justify-center">
-            <Link href="/">
-                <Image src={"/SOELOGO.svg"} width="200%" height="120%"></Image>
-            </Link>
+        <div className="relative pb-24">
+            <div className="flex justify-center fixed top-0 left-0 right-0 bg-white">
+                <div className="cursor-pointer">
+                    <Link href="/">
+                        <Image
+                            src={"/SOELOGO.svg"}
+                            width="180%"
+                            height="100%"
+                        ></Image>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
